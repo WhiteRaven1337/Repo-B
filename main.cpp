89cd63c6ae199b9d299853f7bf542ee92f7cb3a4
Dmitriy <istb-211_413387@volsu.ru>
2022-05-22 18:04:10 +0300

    Изменение файла main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include<iostream>
 #include<std.h>
 using namespace std;
-    int main()
+    int main() {
+      int a, b;
+      cout << "Enter two numbers A and B: \n";
+      cin >> a >> b;
+      cout << "A + B = " << (a+b) << "\n";
+      cout << "A - B = " << (a-b) << "\n";
+      cout << "A * B = " << (a*b) << "\n";
+      cout << "A / B = " << (a/b) << "\n";
+      cout << "A % B = " << (a%b) << "\n";
+      cout << "A | B = " << (a|b) << "\n";
+      cout << "A ^ B = " << (a^b) << "\n";
+      cout << "A & B = " << (a&b) << "\n";
+      cout << "A << B = " << (a<<b) << "\n";
+      cout << "A >> B = " << (a>>b) << "\n";
+
+      cin.get();
+    }
