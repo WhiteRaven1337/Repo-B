dceddaebbde54a88c0c2d5259723891978891673
Dmitriy <istb-211_413387@volsu.ru>
2022-05-25 22:44:25 +0300

    3.1.Произведение чисел
    Signed-off-by: Dmitriy <istb-211_413387@volsu.ru>

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ using namespace std;
       int a, b;
       cout << "Enter two numbers A and B: \n";
       cin >> a >> b;
-      cout << "A - B = " << (a-b) << "\n";
+      cout << "A + B = " << a + b << '\n';
+      cout<< "A - B = " << a - b << '\n';
+      cout<< "A * B = " << a * b << '\n';
       cin.get();
     }
